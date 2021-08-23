2b950fc793ddedfb49d1f8d21765a5c1937aa1b8
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-08-23 07:47:31 +0300

    # чистка

--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -158,72 +158,6 @@ namespace Homework_9
     
     public class TeleBot
     {
-
-        /*             void MessageListener(object sender, MessageEventArgs e)
-            {
-                string text = $"{DateTime.Now.ToLongTimeString()} {e.Message.Chat.FirstName} {e.Message.Chat.Id} {e.Message.Text} {e.Message.Type} end";
-
-                Console.WriteLine(text);
-
-                var message = e.Message.Text;
-                var messageType = e.Message.Type.ToString();
-                Console.WriteLine(message);
-
-                Thread task = new Thread(Download);
-                task.Start();
-
-                if (e.Message.Type == MessageType.Photo)
-                {
-                    Console.WriteLine("загружаю изображение");
-                    Download(e.Message.Photo[^1].FileId);
-                    *//*                    using (var img = new MagickImage("tmp.jpg"))
-                                        {
-                                            // -fuzz XX%
-                                            //img.ColorFuzz = new Percentage(10);
-                                            // -transparent white
-                                            //img.Transparent(MagickColors.White);
-                                            img.Write("image.png");
-                                        }*/
-        /*
-                        }
-
-                        if (e.Message.Text != null)
-                        {
-                            var ee = e.Message.Text.ToLower();
-                            if (ee == "спой")
-                            {
-                                Program.bot.SendTextMessageAsync(e.Message.Chat.Id, "ла ла ла ла");
-                            }
-                            else if (ee == "привет" | ee == "hi")
-                            {
-
-                                Program.bot.SendTextMessageAsync(e.Message.Chat.Id, "Привет!");
-                                var Filename = "hi.jpeg";
-                                var FilePath = Path.Combine(Environment.CurrentDirectory, Filename);
-                                using (var stream = File.OpenRead(FilePath))
-                                {
-                                    var r = Program.bot.SendPhotoAsync(e.Message.Chat.Id, new InputOnlineFile(stream)).Result;
-                                }
-                            }
-                            else if (ee == "опрос")
-                            {
-                                var pollMessage = Program.bot.SendPollAsync(
-                                        e.Message.Chat.Id,
-                                        question: "Нравится бот?",
-                                        options: new[]
-                                        {
-                                                    "да!",
-                                                    "нет"
-                                        }
-                                    );
-                            }
-                            else
-                            {
-                                Program.bot.SendTextMessageAsync(e.Message.Chat.Id, @"Не понял ¯\_(ツ)_/¯");
-                            }
-                        }
-
-        */
         TelegramBotClient bot;
         string token;
 
@@ -258,28 +192,6 @@ namespace Homework_9
                     Console.WriteLine(inputImage);
                     Download(bot, chatId);
                     Console.WriteLine(imageConverter.controlFlag);
-                    //if (imageConverter.controlFlag == true)
-                    //{
-                    //    var FilePath = Path.Combine(Environment.CurrentDirectory, outputImage);
-                    //    using (var stream = File.OpenRead(FilePath))
-                    //    {
-                    //        var r = bot.SendPhotoAsync(e.Message.Chat.Id, new InputOnlineFile(stream)).Result;
-                    //        imageConverter.controlFlag = false;
-                    //    }
-                        
-                    //}
-
-                    //using (FileStream fs = File.Create("tmp"))
-                    //bot.DownloadFileAsync(e.Message.Photo[^1].FileId, fs);
-                    //File.Delete("tmp");
-                    //using (FileStream fs = new FileStream("tmp", FileMode.Create))
-                    //    bot.GetInfoAndDownloadFileAsync(e.Message.Photo[^1].FileId, fs);
-
-
-
-
-                    //Upload();
-
                 }
             }
 
@@ -287,84 +199,5 @@ namespace Homework_9
             bot.StartReceiving();
             Console.ReadKey();
         }
-
-        /*public static string message = null;
-
-        //public static long messageChatId;
-
-        
-
-        
-        static Stream ConvertImage(this Stream originalStream, ImageFormat format)
-        {
-            var image = Image.FromStream(originalStream);
-
-            var stream = new MemoryStream();
-            image.Save(stream, format);
-            stream.Position = 0;
-            return stream;
-        }
-        static void Main()
-        {
-
-             //WebClient wc = new WebClient() { Encoding = Encoding.UTF8 };
-
-             
-             //string startUrl = $@"https://api.telegram.org/bot{token}/";
-             //int update_id = 0;
-             //string photo = @"C:\Users\user0\source\repos\Homeworks\Homework_9\bin\Debug\netcoreapp3.1\hi.jpeg";
-
-            
-
-
-
-            void SavePhoto()
-            {
-
-
-
-
-                string path = "tmp.jpg";
-                Image img = null;
-
-                using (var file = File.Open(path, FileMode.Open, FileAccess.Read))
-                    img = Image.FromStream(file);
-                string path2 = "tmp.png";
-                img.Save(path2, ImageFormat.Png);
-                
-
-                //img.Save(path2, ImageFormat.Png);
-                //img.Dispose();
-
-            *//*                string FileName = "tmp.jpg";
-                            string oFileName = "tmp.png";
-
-                            var image = new MagickImage(FileName);
-
-                                image.Format = MagickFormat.Png;
-                                image.Write(oFileName);*//*
-
-            }
-
-
-
-                async void Download(string id)
-                {
-                    FileStream fs = new FileStream("tmp.jpg", FileMode.Create);
-                    await Program.bot.GetInfoAndDownloadFileAsync(id, fs);
-                    fs.Close();
-                    fs.Dispose();
-                }
-            }
-
-            //SavePhoto();
-
-*/
-
-
-
-            //Console.ReadKey();
-        /*
-                }*/
     }
 }
