daa53deda80b070c58dd15eafb1714b9171ca889
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-08-25 07:57:36 +0300

    # чистка

---
+++ Homework_9/MessageListener.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+
+namespace Homework_9
+{
+    public interface MessageListener
+    {
+        void Listen(object sender, MessageEventArgs e);
+    }
+
+    public class StartMessage : MessageListener
+    {
+        public TelegramBotClient bot;
+        public TelegramBotClient Bot { get; set; }
+
+        public StartMessage(TelegramBotClient Bot)
+        {
+            this.bot = Bot;
+        }
+
+        public void Listen(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Text == "/start")
+            {
+                new SendHelp().SendMessage(e.Message.Chat.Id.ToString(), bot);
+            }
+        }
+    }
+
+    public class ImageMessage : MessageListener
+    {
+        public delegate void ImageSended(object sender, MessageEventArgs e);
+
+        public event ImageSended onPhoto;
+        public void Listen(object sender, MessageEventArgs e)
+        {
+            if (e.Message.Type == MessageType.Photo) onPhoto(sender, e);
+        }
+    }
+
+    public class ImageChecked : MessageListener
+    {
+        public TelegramBotClient bot;
+
+        public TelegramBotClient Bot { get; set; }
+
+        public ImageChecked(TelegramBotClient Bot)
+        {
+            this.bot = Bot;
+        }
+
+        public void Listen(object sender, MessageEventArgs e)
+        {
+            bot.SendTextMessageAsync(e.Message.Chat.Id.ToString(), "Изображение!");
+        }
+    }
+
+}
--- Homework_9/Program.cs
+++ Homework_9/Program.cs
@@ -1,120 +1,46 @@
 ﻿using System;
 using System.Drawing;
 using System.IO;
-using Telegram.Bot;
 using Telegram.Bot.Args;
 using Telegram.Bot.Types.Enums;
-using static Homework_9.imageConverter;
 using static Homework_9.TeleBot;
 using static Homework_9.CallBack;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Homework_9
 {
-    class imageConverter
-    {
-        public TelegramBotClient bot;
-
-        public TelegramBotClient Bot { get; set; }
-    }
-
-    public interface MessageListener
-    {
-        void Listen(object sender, MessageEventArgs e);
-    }
-
-    public class StartMessage : MessageListener
-    {
-        public TelegramBotClient bot;
-        public TelegramBotClient Bot { get; set; }
-
-        public StartMessage(TelegramBotClient Bot)
-        {
-            this.bot = Bot;
-        }
-
-        public void Listen(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Text == "/start")
-            {
-                new SendHelp().SendMessage(e.Message.Chat.Id.ToString(), bot);
-            }
-        }
-    }
-
-    public class ImageMessage
-    {
-        public delegate void ImageSended(object sender, MessageEventArgs e);
-
-        public event ImageSended onPhoto;
-        public void Listen(object sender, MessageEventArgs e)
-        {
-            if (e.Message.Type == MessageType.Photo) onPhoto(sender, e);
-        }
-    }
-
-    public class ImageChecked : MessageListener
-    {
-        public TelegramBotClient bot;
-
-        public TelegramBotClient Bot { get; set; }
-
-        public ImageChecked(TelegramBotClient Bot)
-        {
-            this.bot = Bot;
-        }
-
-        public void Listen(object sender, MessageEventArgs e)
-        {
-            Console.WriteLine("image sended!");
-            //bot.SendTextMessageAsync(e.Message.Chat.Id.ToString(), "Изображение!");
-        }
-    }
 
     class Program
     {
-
-
         public static string inputImage;
         public static string imageName;
         public static string outputImage;
         public static string chatId;
         public static string zippedImage;
         public static bool controlFlag = false;
+        public static bool inputImageExists = false;
 
         [Obsolete]
-        public static async void Scenario(TelegramBotClient bot, string chatId, MessageEventArgs e)
-        {
-
-        }
 
         public static async void SendKeyboard(object sender, MessageEventArgs e)
         {
+            inputImageExists = true;
             chatId = e.Message.Chat.Id.ToString();
-            Console.WriteLine("message received");
             var keyboard = new ReplyKeyboardMarkup
             {
                 Keyboard = new[] {
-    new[] // row 1
-    {
-        new KeyboardButton("BMP"),
-        new KeyboardButton("PNG"),
-        new KeyboardButton("GIF"),
-        new KeyboardButton("TIFF"),
-    },
-},
+                    new[]
+                    {
+                        new KeyboardButton("BMP"),
+                        new KeyboardButton("PNG"),
+                        new KeyboardButton("GIF"),
+                        new KeyboardButton("TIFF"),
+                    },
+                },
                 ResizeKeyboard = true, OneTimeKeyboard = true,
                 
             };
-            await bot.SendTextMessageAsync(chatId, "Выберите формат", replyMarkup: keyboard);
-            
-            //await bot.SendTextMessageAsync(chatId, "Выберите формат в который хотите конвертировать изображение", replyMarkup: CallBack.keyboard());
-            //bot.OnCallbackQuery += ChooseOutputFormat;
-        }
-
-        public static async void RemoveKeyboard(object sender, MessageEventArgs e)
-        {
-
+            await bot.SendTextMessageAsync(chatId, "Выберите формат в который хотите конвертировать изображение", replyMarkup: keyboard);
         }
 
         [Obsolete]
@@ -130,7 +56,7 @@ namespace Homework_9
                 }
                 if (e.Message.Type == MessageType.Photo)
                 {
-                    bot.SendTextMessageAsync(e.Message.Chat.Id, "Обнаружил изображение, запускаем сценарий");
+                    await bot.SendTextMessageAsync(e.Message.Chat.Id, "Обнаружил изображение, запускаем сценарий");
                     inputImage = e.Message.Photo[^1].FileId.ToString();
                     imageName = e.Message.MessageId.ToString();
                     //if (!Directory.Exists(Path.Combine(Environment.CurrentDirectory, chatId))) 
