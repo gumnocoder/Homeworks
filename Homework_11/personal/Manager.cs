c4a50ecc5af9fbdba64c170c8f6ed80a25fe7a50
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-09-10 08:15:53 +0300

    Revert "# upd# Создание Компании - департамента - офиса"
    This reverts commit 13fc18b500e6973064f9f6bbfda25d09fd6d1eb0.

---
+++ Homework_11/personal/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11.personal
+{
+    abstract class Manager
+    {
+        public virtual void Hire()
+        {
+
+        }
+
+        public virtual void Sack()
+        {
+
+        }
+    }
+}
---
+++ Homework_11_console/employe/Director.cs
@@ -0,0 +1,28 @@
+﻿namespace Homework_11_console.employe
+{
+    class Director : Employe
+    {
+        new public string Name { get; set; }
+
+        public static Director _instance;
+        private Director(string Name = "Bill Gates", int Salary = 1300)
+        {
+            base.Salary = Salary;
+            // base.WorkPlace;
+            this.Name = Name;
+        }
+        public static Director companyDirector
+        {
+            get
+            {
+                if (_instance == null) { _instance = new Director(); }
+                return _instance;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Salary}$";
+        }
+    }
+}
