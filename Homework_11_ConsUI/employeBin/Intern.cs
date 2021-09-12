b9a6036fd90b5d9ee9dc87d44fa9fac44ed840f6
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-09-12 17:04:43 +0300

    # Добавлен интерн

---
+++ Homework_11_ConsUI/employeBin/Intern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11_ConsUI.employeBin
+{
+    class Intern : Employe
+    {
+        static int internsCount = 0;
+
+        public Intern(int Salary, string Name, byte Age)
+        {
+            ++internsCount;
+            this.Salary = Salary;
+            this.Name = Name;
+            this.Age = Age;
+        }
+
+        public Intern() : this(100, $"intern_{internsCount}", 18) { ++internsCount; }
+
+        public override string ToString()
+        {
+            return $"{Name} {Age} y.o., {Salary}$ per month";
+        }
+    }
+}
