5da1d0f7c2dcd9be7dd2e3256a29786a16de69f0
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-10-03 23:51:56 +0300

    # заголовок окна

--- Homework_13_bank_system/MainWindow.xaml.cs
+++ Homework_13_bank_system/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using static System.Windows.SystemParameters;
 
 namespace Homework_13_bank_system
 {
@@ -20,9 +21,36 @@ namespace Homework_13_bank_system
     /// </summary>
     public partial class MainWindow : Window
     {
+        void DragAnywhere(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
         public MainWindow()
         {
             InitializeComponent();
+            mainWindowBorder.MouseLeftButtonDown += new MouseButtonEventHandler(DragAnywhere);
+        }
+
+        private void closeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void minBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void maxBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (Width < PrimaryScreenWidth | Height < PrimaryScreenHeight)
+            {
+                Top = 0;
+                Left = 0;
+                Width = PrimaryScreenWidth; 
+                Height = PrimaryScreenHeight; 
+            }  
+            else { Width = 800; Height = 450; }
         }
     }
 }
