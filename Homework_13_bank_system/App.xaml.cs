33e69ab0b3c0ab9ce08b6b1f6bcc9d48c40018c5
gumnocoder <84528014+gumnocoder@users.noreply.github.com>
2021-10-03 23:01:44 +0300

    # 13

--- Homework_13_bank_system/App.xaml.cs
+++ Homework_13_bank_system/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace Homework_13
+namespace Homework_13_bank_system
 {
     /// <summary>
     /// Interaction logic for App.xaml
--- Homework_13_bank_system/MainWindow.xaml.cs
+++ Homework_13_bank_system/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Homework_13
+namespace Homework_13_bank_system
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
